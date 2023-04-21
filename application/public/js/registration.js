033b9df242a373f65b60a524edcfcf17c565b2a9
joseph-shur <jshur1@sfsu.edu>
2023-04-21 15:11:04 -0700

    changed style sheet to general single file, rearranged some js code in the js files

--- application/public/js/registration.js
+++ application/public/js/registration.js
@@ -1,58 +1,16 @@
-const KEY = "abc123blahblahblah";
-
-var inputEmail = document.getElementById("email");
-var inputUname = document.getElementById("uname");
-var inputPW = document.getElementById("pw");
-var inputConfirmPW = document.getElementById("confirmpw");
-function submitForm(inputEmail, inputUname, inputPW, inputConfirmPW) {
-
-}
-
-function checkForm() {
-    if (checkEmail === true) {
-        if (checkUname() === true) {
-            if (checkPassword() === true) {
-                if (checkConfirmPassword() === true) {
-                    
-                }
-            }
-        }
+document.getElementById("email").addEventListener("input", function (ev) {
+    let userInput = ev.currentTarget;
+    let username = userInput.value;
+    if (username.length >= 3) {
+        userInput.style.color = "green";
+    } else {
+        userInput.style.color = "red";
     }
+});
+
+function validateForm() {
+    return true;
+
+
 }
 
-function checkEmail() {
-    for (let i = 0; i < inputEmail.length; i++) {
-        if (inputEmail[i] === "@") {
-            return true
-        } else {
-            return false
-        }
-    }
-}
-
-function checkUname() {
-    if (inputUname[0].value.match(/[a-z]/g)) {
-        if (inputUname[0].value.match(/[A-Z]/g)) {
-            if (inputUname.length >= 3) {
-                return true
-            }
-        }
-    }
-}
-
-function checkPassword() {
-    if (inputPW.length >= 8) {
-        for (let i = 0; i < inputPW.length; i++) {
-            if (inputPW[i].value.match(/[0-9]/g)) {
-                return true
-            } else {
-                return false
-            }
-        }
-    }
-}
-function checkConfirmPassword() {
-    if (inputConfirmPW.value = inputPW.value) {
-        return true
-    }
-}
