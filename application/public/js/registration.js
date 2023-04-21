ef2dff7b60e825e8709ca6a9d14df4a3105be555
joseph-shur <jshur1@sfsu.edu>
2023-04-21 11:53:29 -0700

    added checkEmail function and cleaned up other functions

--- application/public/js/registration.js
+++ application/public/js/registration.js
@@ -4,27 +4,55 @@ var inputEmail = document.getElementById("email");
 var inputUname = document.getElementById("uname");
 var inputPW = document.getElementById("pw");
 var inputConfirmPW = document.getElementById("confirmpw");
-function checkInput(inputEmail, inputUname, inputPW, inputConfirmPW) {
-    
+function submitForm(inputEmail, inputUname, inputPW, inputConfirmPW) {
+
 }
 
-inputPW.onkeyup = function() {
+function checkForm() {
+    if (checkEmail === true) {
+        if (checkUname() === true) {
+            if (checkPassword() === true) {
+                if (checkConfirmPassword() === true) {
+                    
+                }
+            }
+        }
+    }
+}
 
+function checkEmail() {
+    for (let i = 0; i < inputEmail.length; i++) {
+        if (inputEmail[i] === "@") {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+function checkUname() {
     if (inputUname[0].value.match(/[a-z]/g)) {
         if (inputUname[0].value.match(/[A-Z]/g)) {
             if (inputUname.length >= 3) {
-
+                return true
             }
         }
     }
+}
 
+function checkPassword() {
     if (inputPW.length >= 8) {
         for (let i = 0; i < inputPW.length; i++) {
             if (inputPW[i].value.match(/[0-9]/g)) {
-                i = 8;
+                return true
             } else {
+                return false
             }
         }
     }
-
+}
+function checkConfirmPassword() {
+    if (inputConfirmPW.value = inputPW.value) {
+        return true
+    }
 }
